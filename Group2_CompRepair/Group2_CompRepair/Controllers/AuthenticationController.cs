a182366a7dd2e09dea20770a0829b36274884aa5
Jacob <vb2553yq@go.minnstate.edu>
2022-04-25 18:33:39 -0500

    Updated version with unit tests

---
+++ Group2_CompRepair/Group2_CompRepair/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Group2_CompRepair.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly JwtAuthenticationManager jwtAuthenticationManager;
+
+        public AuthenticationController(JwtAuthenticationManager jwtAuthenticationManager)
+        {
+            this.jwtAuthenticationManager = jwtAuthenticationManager;
+        }
+
+
+        [HttpPost]
+        public IActionResult AuthUser([FromBody] User user)
+        {
+            var token = jwtAuthenticationManager.Authentication(user.username, user.password);
+            if(token == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(token);
+        }
+
+    }
+
+    public class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ Group2_CompRepair/Group2_CompRepairTests/UnitTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group2_CompRepair;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Group2_CompRepair.Controllers;
+using Group2_CompRepair.Models;
+
+namespace Group2_CompRepair.Tests
+{
+    [TestClass()]
+    public class UnitTests
+    {
+        [TestMethod()]
+        public void AuthenticationFailTest()
+        {
+            JwtAuthenticationManager manager = new JwtAuthenticationManager("thisisthekey1234");
+
+            User user = new User
+            {
+                username = "username12",
+                password = "password123"
+            };
+
+            var ret = manager.Authentication(user.username, user.password);
+
+            Assert.IsNull(ret);
+        }
+
+        [TestMethod()]
+        public void AuthenticationSucceedTest()
+        {
+            JwtAuthenticationManager manager = new JwtAuthenticationManager("thisisthekey1234");
+
+            User user = new User
+            {
+                username = "admin",
+                password = "password"
+            };
+
+            var ret = manager.Authentication(user.username, user.password);
+
+            Assert.IsNotNull(ret);
+        }
+
+        [TestMethod()]
+        public void EmployeesTest()
+        {
+            EmployeesController employees = new EmployeesController();
+
+            Assert.IsNotNull(employees.GetEmployees());
+        }
+
+        [TestMethod()]
+        public void CustomersTest()
+        {
+            CustomersController customers = new CustomersController();
+
+            Assert.IsInstanceOfType(customers.GetCustomers().Id, typeof(int));
+        }
+
+        [TestMethod()]
+        public void SoftwareTest()
+        {
+            SoftwaresController software = new SoftwaresController();
+
+            Software ware = new Software
+            {
+                SoftwareName = "TestWare",
+                SoftwareDescription = "This is a test",
+                LicensesInStock = 5,
+                SoftwareType = "Test",
+                SoftwarePrice = (decimal?)1000.0
+            };
+
+            Assert.IsTrue(ware.SoftwarePrice > 800);
+        }
+    }
+}
